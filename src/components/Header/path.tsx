6fee749c6f63c172c38ac45afa2ec108405da43b
Ankit.Ghildiyal@123 <ce19b005@iittp.ac.in>
2022-07-25 14:08:01 +0530

    Modified code for path shown in header

--- src/components/Header/path.tsx
+++ src/components/Header/path.tsx
@@ -2,6 +2,9 @@ export default function getPath(props : string) {
   let path : string = props;
   if(path.at(-1) === "/")
     path = path.slice(0, -1);
-  path = 'root' + path.replaceAll('/', ' 〉');
+
+  path = path.slice(1, path.length);
+
+  path = path.replaceAll('/', ' 〉');
   return path;
 }
