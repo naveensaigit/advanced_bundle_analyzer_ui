96c0c953bfdb4fc2b2d792815b07fdfb746dcedc
Ankit.Ghildiyal@123 <ce19b005@iittp.ac.in>
2022-08-02 10:10:29 +0530

    Added modal feature

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -5,8 +5,10 @@ import originalDataObj from "../data.json";
 import FileDetails from "./FileDetails";
 import { data } from "../Browser/Browser";
 import { UpdateDataObj } from "../Browser/Browser";
-import DeleteIcon from '@mui/icons-material/Delete';
-import IconButton from "@mui/material/IconButton";
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import { useRouter } from 'next/router';
 
 let dataObj: data = JSON.parse(JSON.stringify(originalDataObj));
@@ -21,17 +23,17 @@ if (typeof window !== "undefined") {
   });
 }
 
-function clearSavedPreferences() : void{
+function clearSavedPreferences(): void {
   dataObj = JSON.parse(JSON.stringify(originalDataObj));
 
-  for(let path in dataObj){
-    if(typeof dataObj[path].canBeLazyLoaded  === "object"){
+  for (let path in dataObj) {
+    if (typeof dataObj[path].canBeLazyLoaded === "object") {
       let imp = dataObj[path].canBeLazyLoaded;
 
-      if(typeof imp === "number")   continue;
+      if (typeof imp === "number") continue;
 
-      for(let suggestion in imp){
-        if(localStorage.getItem(imp[suggestion].path + ":" + imp[suggestion].exportName)){
+      for (let suggestion in imp) {
+        if (localStorage.getItem(imp[suggestion].path + ":" + imp[suggestion].exportName)) {
           localStorage.removeItem(imp[suggestion].path + ":" + imp[suggestion].exportName);
         }
       }
@@ -41,23 +43,60 @@ function clearSavedPreferences() : void{
   window.dispatchEvent(new Event("clearPreferences"));
 }
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 380,
+  bgcolor: '#282828',
+  border: '0.5px solid white',
+  boxShadow: 24,
+  pt: 4,
+  pl: 4,
+  pr: 4,
+  pb: 3,
+};
+
 export default function Review({ route }: { route: string }) {
-  if (typeof window !== "undefined")  window.dispatchEvent(new Event("storage"));
-  
+  if (typeof window !== "undefined") window.dispatchEvent(new Event("storage"));
+
   const router = useRouter();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ color: "#E1245E" }}>
+            Notification
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2, color: "white", mb: 4 }}>
+            Clearing the saved preferences for removed suggestions will cause this page to reload. 
+          </Typography>
+          <Button variant="outlined" color="success" sx={{ ml: 14.5, fontWeight:'700' }} onClick={() => {
+            clearSavedPreferences();
+            router.reload();
+          }}>
+            Clear
+          </Button>
+        </Box>
+      </Modal>
       <Header route={route + " : review"} />
       <DoughnutChartComponent dataObj={dataObj} route={route} />
+      <Button variant="outlined" color="success" sx={{ mb: 5, fontWeight:'700' }} onClick={() => {
+        handleOpen();
+      }}>
+        Clear Saved Preferences
+      </Button>
       <FileDetails dataObj={dataObj} route={route} />
-      <IconButton
-        onClick={() => {
-          clearSavedPreferences();
-          router.push("/browse");
-        }}
-        size="large">
-        <DeleteIcon sx={{ color: "white" }} />
-      </IconButton>
     </>
   );
 }
--- src/components/Review/TableGenerator.tsx
+++ src/components/Review/TableGenerator.tsx
@@ -8,21 +8,45 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import ClearIcon from '@mui/icons-material/Clear';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import { data } from '../Browser/Browser'
 import { canBeLazy } from "../Browser/Browser";
 import { useRouter } from 'next/router';
 
-function myFunc(nonlazy: number | canBeLazy, row: string) {
+let currentRow: string = "";
+
+function RemoveSuggestion(nonlazy: number | canBeLazy, row: string) {
   if (!nonlazy || typeof nonlazy === "number") return
 
-  let key: string = nonlazy[row].path + ':' + nonlazy[row].exportName;
+  let key: string = nonlazy[currentRow].path + ':' + nonlazy[currentRow].exportName;
   window.localStorage.setItem(key, "true");
   window.dispatchEvent(new Event("storage"));
 }
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 380,
+  bgcolor: '#282828',
+  border: '0.5px solid white',
+  boxShadow: 24,
+  pt: 4,
+  pl: 4,
+  pr: 4,
+  pb: 3,
+};
 
 export default function BasicTable({ dataObj, subFiles }: { dataObj: data; subFiles: string[]; }) {
   const router = useRouter();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
       {
@@ -31,9 +55,31 @@ export default function BasicTable({ dataObj, subFiles }: { dataObj: data; subFi
           let can: number = 0, already: number = 0;
           return (typeof lazy === "number" && typeof nonlazy === "object" && Object.keys(nonlazy).length > 0) ?
             <div key={file}>
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={style}>
+                  <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ color: "#E1245E" }}>
+                    Notification
+                  </Typography>
+                  <Typography id="modal-modal-description" sx={{ mt: 2, color: "white", mb: 4 }}>
+                  Removing a suggestion will cause you to re route to the root directory.
+                  </Typography>
+                  <Button variant="outlined" color="error" sx={{ ml: 7.5, fontWeight:'700'  }} onClick={() => {
+                    RemoveSuggestion(nonlazy, currentRow);
+                    router.push("/browse");
+                  }}>
+                    Remove Suggestion
+                  </Button>
+                </Box>
+              </Modal>
+
               <TableContainer component={Paper} sx={{ width: "95%", margin: "auto", marginBottom: 5, borderRadius: 5, overflow: 'hidden' }}>
                 <Table sx={{ minWidth: 65, borderRadius: 100 }} aria-label="simple table">
-                  <TableHead sx={{ backgroundColor: "#1C1C1F", borderBottom: '2px solid black' }}>
+                  <TableHead sx={{ backgroundColor: "#1C1C1F", borderBottom: '2px solid black', height:40 }}>
                     <TableRow>
                       <TableCell sx={{ width: 80, minWidth: 80, color: "white", fontSize: 22 }}>
                         <span style={{ color: '#34AC36', fontWeight: 700 }}>{lazy}</span> - <span style={{ color: '#E1245E', fontWeight: 700 }}>{Object.keys(Object.keys(nonlazy)).length}</span>
@@ -43,36 +89,27 @@ export default function BasicTable({ dataObj, subFiles }: { dataObj: data; subFi
                       <TableCell align="center" sx={{ color: "white", fontSize: 15, width: 70 }} >Delete</TableCell>
                     </TableRow>
                   </TableHead>
-                  <TableBody sx={{ fontSize: 200 }}>
+                  <TableBody >
                     {Object.keys(nonlazy).map((row: string) => {
                       if (typeof nonlazy === "number") return <></>;
                       return (
                         <TableRow key={row} sx={{ backgroundColor: "#E1245E", borderBottom: '2px solid black' }} >
-                          <TableCell component="th" scope="row" align="center" sx={{ fontSize: 18, fontWeight: 550 }}>
+                          <TableCell component="th" scope="row" align="center" sx={{ fontSize: 17, fontWeight: 400, padding:'10px' }}>
                             {(++can + already)}.
                           </TableCell>
-                          <TableCell sx={{ borderLeft: '2px solid black', fontSize: 18, fontWeight: 550 }}>{row}</TableCell>
-                          <TableCell sx={{ borderLeft: '2px solid black', fontSize: 18, fontWeight: 550, whiteSpace: 'nowrap' }}>{nonlazy[row].path}</TableCell>
-                          <TableCell align="center" sx={{ borderLeft: '2px solid black', fontSize: 18, fontWeight: 550 }}>
-                            <IconButton onClick={() => {
-                              myFunc(nonlazy, row);
-                              router.push("/browse");
+                          <TableCell sx={{ borderLeft: '2px solid black', fontSize: 17, fontWeight: 400, padding:'10px' }}>{row}</TableCell>
+                          <TableCell sx={{ borderLeft: '2px solid black', fontSize: 17, fontWeight: 400, whiteSpace: 'nowrap', padding:'10px'}}>{nonlazy[row].path}</TableCell>
+                          <TableCell align="center" sx={{ borderLeft: '2px solid black', fontSize: 17, fontWeight: 400, padding:'10px' }}>
+                          <IconButton onClick={() => {
+                              currentRow = row;
+                              handleOpen();}
                             }
-                            } size="large">
-                              <ClearIcon sx={{ color: 'black' }} />
+                              size="small">
+                              <ClearIcon sx={{ color: '#323232'}} />
                             </IconButton>
                           </TableCell>
                         </TableRow>);
                     })}
-                    {/* {lazy.map((row : row) => (
-                      <TableRow key={row.name} sx={{ backgroundColor: "#34AC36", borderBottom: '2px solid black' }} >
-                        <TableCell component="th" scope="row" align="center" sx={{fontSize:18, fontWeight:550}}>
-                          {(++already + can)}.
-                        </TableCell>
-                        <TableCell sx={{ borderLeft: '2px solid black', fontSize:18, fontWeight:550 }}>{row.name}</TableCell>
-                        <TableCell sx={{ borderLeft: '2px solid black', fontSize:18, fontWeight:550 }}>{row.path}</TableCell>
-                      </TableRow>
-                    ))} */}
                   </TableBody>
                 </Table>
               </TableContainer>
